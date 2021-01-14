c4a430d8e83f8c5f8df079614645040cf3b865bf
bruteForceBilly <bruteForceBilly@protonmail.com>
2021-01-14 21:06:40 +0100

    puppeteer gets html and css

---
+++ src/scrape.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+const fetchCss = require("fetch-css");
+
+
+const getContent = async function(arg) {
+let result = {};
+let styleSheet;
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(arg);
+
+    result.html = await page.evaluate(() => {
+        styleSheet = document.head.querySelector("link[rel='stylesheet']").href
+        return document.documentElement.outerHTML
+    });
+
+    const [{css}] = await fetchCss([{url: "https://bruteforcebilly.github.io/hyf-fest/style.css" }]);
+
+    result.css = css;
+
+    //result = await page.content()
+    await browser.close();
+  } catch(error) {
+    result = error;
+    console.warn(`We have an error here: ${error}`)
+  }
+  return result
+};
+
+const scrape = (arg) => {
+    return new Promise((resolve) => {
+        getContent(arg).then((result) => {
+            console.log("result", result)
+            resolve(result)
+    }).catch(e => {
+        console.error(e)
+    });
+})
+}
+
+module.exports = {
+    scrape: scrape
+}
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const { scrape } = require('./scrape.js');
+
+app.get('/', (req, res) => {
+  res.send('Welcome to FEST 🎉')
+})
+
+app.get('/unit/:src', (req, res) => {
+  console.log("get", req.params.src)
+  scrape(req.params.src).then((result) => {
+    res.send(result)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
