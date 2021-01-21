41854d80c48241717a5d550115c7b47d97d188a2
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2021-01-21 22:04:48 +0100

    initial

--- app.js
+++ app.js
@@ -1,47 +1,39 @@
-const express = require("express");
+const express = require('express');
 const { validator } = require('./src/validator.js');
 
 const app = express();
 
 let port = process.env.PORT;
 
-if (port == null || port == "") {
+if (port == null || port == '') {
   port = 8000;
 }
 
-
 const normalizeUrl = require('normalize-url');
 
-
-const regexUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-
+const regexUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to FEST 🎉')
-})
+  res.send('Welcome to FEST 🎉');
+});
 
 app.get('/run', async (req, res) => {
-
   let url = null;
 
-  if(Object.keys(req.query).includes("url")){
-    url = normalizeUrl(req.query.url)
+  if (Object.keys(req.query).includes('url')) {
+    url = normalizeUrl(req.query.url);
   } else {
-    res.status(400).send("Invalid query param key")
+    res.status(400).send('Invalid query param key');
   }
-  
-  if(regexUrl.test(url)){
+
+  if (regexUrl.test(url)) {
     const result = await validator(url);
-    res.send(result);
-
+    res.json(result);
   } else {
-    res.status(400).send("Invalid query param value")
+    res.status(400).send('Invalid query param value');
   }
-
-
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ cli/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const normalizeUrl = require('normalize-url');
+
+const { validator } = require('../src/validator.js');
+
+const students = require('./students.json');
+
+// will default to evaluating their home page
+//  try acme-web-design, this was an assigned project repo
+const repoName = process.argv[2] || '';
+console.log('evaluating: ', repoName);
+
+const resultsPath = path.join(__dirname, 'results');
+if (!fs.existsSync(resultsPath)) {
+  fs.mkdirSync(resultsPath);
+}
+for (const student of students) {
+  const origin = `https://${student.userName}.github.io/`;
+  const url = normalizeUrl(origin + repoName);
+  const className = student.className;
+
+  console.log('... validatoring ', student.name, ', ', url);
+
+  validator(url).then(result => {
+    const classDir = path.join(resultsPath, className);
+    if (!fs.existsSync(classDir)) {
+      fs.mkdirSync(classDir);
+    }
+
+    const studentDir = path.join(classDir, student.userName);
+    if (!fs.existsSync(studentDir)) {
+      fs.mkdirSync(studentDir);
+    }
+
+    console.log('... writing ', student.name);
+    if (result) {
+      const outputPath = path.join(
+        studentDir,
+        // the JSON is not valid. huh
+        `${repoName || student.userName + '.github.io'}.json`
+      );
+      fs.writeFile(outputPath, result, err => (err ? console.log(err) : null));
+    } else {
+      const outputPath = path.join(
+        studentDir,
+        `${repoName || student.userName + '.github.io'}.NO_RESULT.txt`
+      );
+      fs.writeFile(outputPath, '', err => (err ? console.log(err) : null));
+    }
+  });
+}
